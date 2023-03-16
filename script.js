0a981eed1ee76188399fa306dbfa8055c57e23d5
boboraul <raul_de_asiss@yahoo.com>
2023-03-16 20:05:20 +0200

    3rd commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 let countdownTitle = '';
 let countdownDate = '';
-let countdownValue = Date;
+let countdownValue = Date();
 let countdownActive;
 let savedCountdown;
 
